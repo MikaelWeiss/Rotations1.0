cdc1c143d10f7ec4961c3187f583be9e2b4b8213
MikaelWeiss <campingmikael@icloud.com>
2017-06-01 05:40:55 -0600

    HELLO

---
+++ Rotations/RotationScreen.swift
@@ -0,0 +1,44 @@
+//
+//  RotationScreen.swift
+//  Rotations
+//
+//  Created by Mikael Weiss on 5/31/17.
+//  Copyright © 2017 MikeStudios. All rights reserved.
+//
+
+import UIKit
+
+class RotationScreen: UIViewController,UITableViewDataSource, UITableViewDelegate {
+    var groupArray = [""]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return groupArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GroupCell", for: indexPath)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            groupArray.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+        }
+    }
+
+
+}
